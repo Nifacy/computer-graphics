8a7701c75d7eeb62aac9ad816b1002ef9965bced
Nifacy <mai.grishin.alexey@gmail.com>
2023-09-30 01:11:26 +0300

    add graphic widget

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from typing import Callable
 import numpy as np
 import sys
 
-from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QSize, Qt, QPoint
+from PyQt5.QtGui import QPainter, QPaintEvent, QPen, QMouseEvent
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame
 
 
 def points_calculator(a: float) -> Callable[[np.arange], np.ndarray]:
@@ -17,14 +18,49 @@ def points_calculator(a: float) -> Callable[[np.arange], np.ndarray]:
     return calculate_values
 
 
+class GraphicWidget(QFrame):
+    def __init__(self, parent: QWidget | None):
+        super().__init__(parent)
+        self.setStyleSheet('background-color: grey;')
+        self.__points: np.ndarray = np.empty((0, 2))
+
+    def __draw_points(self, painter: QPainter) -> None:
+        pen = QPen(Qt.black, 4, Qt.SolidLine)
+        painter.setPen(pen)
+
+        for index in range(len(self.__points) - 1):
+            start_point = self.__points[index]
+            end_point = self.__points[index + 1]
+            painter.drawLine(
+                int(start_point[0]), int(start_point[1]),
+                int(end_point[0]), int(end_point[1]),
+            )
+
+    def __get_painter(self) -> QPainter:
+        painter = QPainter()
+        painter.setRenderHint(QPainter.Antialiasing)
+        return painter
+
+    def draw_points(self, points: np.ndarray) -> None:
+        self.__points = points
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        painter = self.__get_painter()
+        painter.begin(self)
+        self.__draw_points(painter)
+        painter.end()
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.__calculate_points = points_calculator(100)
         self.setFixedSize(QSize(400, 400))
-        self.init_widgets()
+        self.__init_widgets()
 
-    def init_widgets(self):
-        pass
+    def __init_widgets(self):
+        self.__graphic = GraphicWidget(self)
+        self.__graphic.setFixedSize(QSize(400, 400))
+        self.__graphic.draw_points(self.__calculate_points(np.arange(0, np.pi, 0.001)))
 
 
 def main():
