ee4bc567a91a0e8ba193d82c4e3d3493c4936827
Nifacy <mai.grishin.alexey@gmail.com>
2023-09-29 23:48:22 +0300

    wrap calculate function in factory

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from typing import Callable
 import numpy as np
 
 
-def calculate_values(arguments: np.arange, a: float) -> np.ndarray:
-    sin_phi = np.sin(arguments)
-    cos_phi = np.cos(arguments)
-    r_values = 2 * a * sin_phi * cos_phi
-    x_values = r_values * cos_phi
-    y_values = r_values * sin_phi
-    return np.array([x_values, y_values])
-
-
+def points_calculator(a: float) -> Callable[[np.arange], np.ndarray]:
+    def calculate_values(arguments: np.arange) -> np.ndarray:
+        sin_phi = np.sin(arguments)
+        cos_phi = np.cos(arguments)
+        r_values = 2 * a * sin_phi * cos_phi
+        x_values = r_values * cos_phi
+        y_values = r_values * sin_phi
+        return np.array([x_values, y_values]).transpose()
+    return calculate_values
