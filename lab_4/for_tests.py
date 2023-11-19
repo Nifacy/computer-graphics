f27682db671b528cc8069517e96a818250f3cd79
Nifacy <mai.grishin.alexey@gmail.com>
2023-11-19 19:44:56 +0300

    for-tests: added z buffer and perspective proj

--- lab_4/for_tests.py
+++ lab_4/for_tests.py
@@ -4,41 +4,55 @@ import moderngl
 from collections import namedtuple
 import numpy as np
 from PIL import Image
+from OpenGL import GL
 
-# Определение структуры Point
-Point = namedtuple('Point', ['x', 'y'])
-Triangle = tuple[Point, Point, Point]
+# Определение структуры ColoredPoint и ColoredTriangle
+ColoredPoint = namedtuple('ColoredPoint', ['x', 'y', 'z', 'r', 'g', 'b'])
+ColoredTriangle = tuple[ColoredPoint, ColoredPoint, ColoredPoint]
 
-def dump_triangles(triangles: Iterable[Triangle]) -> np.array:
+def dump_colored_triangles(triangles: Iterable[ColoredTriangle]) -> np.array:
     return np.concatenate(triangles, axis=0).astype('f4')
 
-
-def draw_triangle(triangles: Iterable[Triangle], canvas_size: tuple[int, int]) -> np.ndarray:
+def draw_colored_triangle(triangles: Iterable[ColoredTriangle], canvas_size: tuple[int, int]) -> np.ndarray:
     ctx = moderngl.create_standalone_context()
 
-    vbo = ctx.buffer(dump_triangles(triangles))
     prog = ctx.program(
         vertex_shader='''
             #version 330
-            in vec2 in_vert;
+
+            in vec3 in_vert;
+            in vec3 in_color;
+            out vec3 frag_vert;
+            out vec3 frag_color;
+
             void main() {
-                gl_Position = vec4(in_vert, 0.0, 1.0);
+                frag_vert = in_vert;
+                frag_color = in_color;
+                gl_Position = vec4(in_vert.x / in_vert.z, in_vert.y / in_vert.z, in_vert.z, 1.0);
             }
         ''',
         fragment_shader='''
             #version 330
+            in vec3 frag_vert;
+            in vec3 frag_color;
             out vec4 color;
             void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);  // Красный цвет
+                color = vec4(frag_color, 1.0); 
             }
         ''',
     )
 
-    frame_buffer = ctx.simple_framebuffer(canvas_size)
+    vbo = ctx.buffer(dump_colored_triangles(triangles))
+    frame_buffer = ctx.framebuffer(
+        color_attachments=ctx.texture(canvas_size, 4),
+        depth_attachment=ctx.depth_renderbuffer(canvas_size),    
+    )
 
-    vao = ctx.simple_vertex_array(prog, vbo, 'in_vert')
+    vao = ctx.simple_vertex_array(prog, vbo, 'in_vert', 'in_color')
     frame_buffer.use()
-    frame_buffer.clear(0.0, 0.0, 0.0, 1.0)
+    frame_buffer.clear(1.0, 1.0, 1.0, 0.0)
+
+    GL.glEnable(GL.GL_DEPTH_TEST)
     vao.render(moderngl.TRIANGLES)
 
     data = np.frombuffer(frame_buffer.read(), dtype=np.uint8)
@@ -47,10 +61,10 @@ def draw_triangle(triangles: Iterable[Triangle], canvas_size: tuple[int, int]) -
 
 
 triangles = [
-    (Point(-0.2, -0.2), Point(0.0, 0.2), Point(0.2, -0.2)),
-    (Point(0.3, -0.2), Point(0.5, 0.2), Point(0.7, -0.2)),
+    (ColoredPoint(-0.1, -0.2, 0.5, 1.0, 0.0, 0.0), ColoredPoint(0.1, 0.2, 0.5, 1.0, 0.0, 0.0), ColoredPoint(0.3, -0.2, 0.5, 1.0, 0.0, 0.0)),
+    (ColoredPoint(0.0, -0.2, 0.3, 0.0, 1.0, 0.0), ColoredPoint(0.1, 0.2, 0.55, 0.0, 1.0, 0.0), ColoredPoint(0.3, -0.2, 0.55, 0.0, 1.0, 0.0)),
 ]
 
-data = draw_triangle(triangles, (400, 400))
+data = draw_colored_triangle(triangles, (400, 400))
 image = Image.fromarray(data, 'RGB')
 image.show()
